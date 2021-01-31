1a57c2340e9182d3193a07aad5878c287ff044a9
Kody Samaroo <github email address>
2021-01-30 22:50:56 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  let hour = parseInt(string);
+  if (hour > 0 && hour < 12) {
+    return "Good Morning";
+  } else if (hour > 12 && hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
